3931f05cfefee9c660db85bb09b8f15d501b6023
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-07-22 18:13:38 -0700

    dmx rapping

--- server.js
+++ server.js
@@ -16,16 +16,6 @@ const { PORT, DATABASE_URL, JWT_SECRET, JWT_EXPIRY, PETFINDER_CLIENT_ID, PETFIND
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.options('*', cors())
-// app.use(function(req, res, next){
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-// 	if (req.method === 'OPTIONS') {
-// 		return res.send(204);
-// 	}
-// 	next();
-// });
 
 
 app.use(express.static('public'));
@@ -47,48 +37,6 @@ const logErrors = (err, req, res, next) => {
 
 
 app.use(logErrors);
-
-// let token;
-
-// const generatePetFinderToken = () => {
-//     //we need to run
-//             // xios({
-//             //     url: `${mAPI}/characters?nameStartsWith=${query}`,
-//             //     method: "GET",
-//             //     params:{
-//             //         "apikey": `${mPublicKey}`,
-//             //         "ts": `${timeStamp}`,
-//             //         "hash": `${hash}`
-//             //     },
-//             //     headers: {
-//             //         "accept": "application/json",
-//             //     }
-//     //$ curl -d "grant_type=client_credentials&client_id=XgCPNJwDy9c4aedC6NO3bR3f7FaZJyjxkWFc7dp4Mcl4wwj2Rs&client_secret=sITbiLaXhFikNjnjW8QNJAgBWjMp6C09OksmLDqj" https://api.petfinder.com/v2/oauth2/token
-//     return axios({
-//         url: 'https://api.petfinder.com/v2/oauth2/token',
-//         method: 'POST',
-//         data: {
-//             "grant_type":"client_credentials",
-//             "client_id": PETFINDER_CLIENT_ID,
-//             "client_secret": PETFINDER_CLIENT_SECRET
-//         },
-//         headers: {
-//             "accept": "application/json"
-//         }
-
-
-//     })
-//     .then( (response) =>{
-//         console.log('////////////////////////////////////////////////////');
-//         console.log(response);
-//         // process.env.PETFINDER_TOKEN = response.access_token;
-//         token =  response.access_token
-        
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-// }
 
 let server;
 
