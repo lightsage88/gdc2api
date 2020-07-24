492708468d52110b2ce11a8b8587a1fc83173792
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-07-24 01:45:12 -0700

    just adding lines

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.use(morgan('common'));
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/cats', catRouter);
+app.options('/api/auth', cors())
+app.options('/api/users', cors())
+app.options('/api/cats', cors())
+
 const logErrors = (err, req, res, next) => {
     console.error(err);
     return res.status(500).json({Error: 'Something went awry'});
